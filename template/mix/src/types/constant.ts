3eb8062b82491928179f636f1d4709a49585d4ac
ZhangLeo <442623641@qq.com>
2021-11-09 18:13:05 +0800

    template sidemenu,tabs,mix

--- template/mix/src/types/constant.ts
+++ template/mix/src/types/constant.ts
@@ -1,5 +1,5 @@
 
-export const USER_ROLES = { 1: '学生处', 2: '辅导员', 3: '班主任', 4: '信息查看员', 5: '分院审核员', 6: '教务处', 8: '管理员' }
+export const USER_ROLES = { 1: '学生处', 2: '辅导员', 3: '班主任', 5: '教务处', 6: '查看员', 8: '管理员' }
 export const APP_NAME = 'vue-typescript-admin-template'
 
 // 网站默认设置
